899d33d06e7e61a1ac5c4cf0b432ebbcd79baf9e
Joe <jeric720@icloud.com>
2020-05-16 09:46:00 -0400

    initial commit

---
+++ src/lesson_one/Lesson1.java
@@ -0,0 +1,30 @@
+/*
+ * Lesson one of Tim Buchalka's Java Course on Udemy
+ * 
+ * Lesson covers Keywords and Expressions
+ */
+
+package lesson_one;
+
+public class Lesson1 {
+	
+	public static void main(String[] args) {
+		
+		// A mile = 1.609344 kilometers
+		
+		double kilometers = (100 * 1.609344 ); // expression does not include type. 		
+		int highScore = 50;
+		
+		//control flow statement with expression
+		
+		if (highScore == 50) { // component in parenthesis is the expression
+			
+			System.out.println("This is an expression as well");
+			
+		}
+		
+		
+
+	}
+
+}
