28950d04d3c32794740b26213552985cf1a97565
Joe <jeric720@icloud.com>
2020-05-16 10:05:08 -0400

    examples of white space and indenting

---
+++ src/lesson_one/Lesson2.java
@@ -0,0 +1,37 @@
+
+// Statements, Whitespace and Indenting
+
+package lesson_one;
+
+public class Lesson2 {
+	
+	public static void main(String[] args) {
+		
+		// Statements are needed to complete a Java line. 
+		// indenting is needed for logical flow and readability 
+		
+		int 
+		myVariable // Java will ignore whitespace. 
+		= 
+		50; 
+		
+		myVariable++;
+		myVariable--;
+		
+		
+		System.out.println("This is a method call");
+		
+		System.out.println("This " +  // this is a good example of how java handles concatenation and white space
+							"is another " + 
+							"method call " +
+							"illustrating white space " + 
+							"and concatenation");
+		
+		
+		int myVar = 9; myVar--; // statements can be on one line, though this is not preferred due to organization standards
+		
+		
+	}
+	
+
+}
